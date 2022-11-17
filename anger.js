01ce2e83e1e74c4fafee88cca96a47a8c63ab775
Foster Sullivan <fostersullivan12@gmail.com>
2022-11-17 14:29:35 -0500

    Functional, checks for the users page

--- anger.js
+++ anger.js
@@ -4,27 +4,41 @@
  * Extension Idea: Angry professors show up whenever you switch tabs to make sure you're still focused on becoming SWE.
 
 (lunch 1hour)
- * [] Iterate through instructor faces
- * [] changing the text on the bubble (in the am)
- * [] Fix scroll bug
- * [] add pseudo code 
- * [] add Readme.md
+ * [done] Iterate through instructor faces
+ * [done] changing the text on the bubble (in the am)
  * 
  * Stretch
- * [] Only show up when not on productive sites ~ 1hr
+ * [done] Only show up when not on productive sites ~ 1hr
  * [] Every time tabs are switched make a sound?
  * [] Optimize image movement (currently real laggy)
  * [] Currently the curser is gone and we just have the img follow the position of the invisible cursor
+ * [] Fix scroll bug
+ * [] add Readme.md
  *    -> would like to have the cursor BE the img. 
  */
 
+// get the current tabs title element. check to see if it it includes Coding, GitHub, LeetCode, MDN, Calendar, 
+
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Element/title
+
+const allAngryTeachers = [
+  'https://i.imgur.com/xiG22Yw.png',
+  'https://i.imgur.com/vFVfddo.png',
+  'https://i.imgur.com/xB7QEbs.png',
+  'https://i.imgur.com/ut5Z65t.png',
+  'https://i.imgur.com/5QOVBK2.png',
+  'https://i.imgur.com/GsXFUkf.png',
+  'https://i.imgur.com/68lSEC3.png',
+  'https://i.imgur.com/fzLtKY3.png',
+];
+
 const body = document.querySelector('body');
-const cursorSrc = 'https://i.imgur.com/hlOtvGY.png';
+const cursorSrc = allAngryTeachers[Math.floor(Math.random() * 7)];
 const cursor = document.createElement('img');
 
 // Setting cursor CSS styling and image source
 cursor.setAttribute('src', cursorSrc);
-cursor.style.height = '100px';
+cursor.style.height = '200px';
 cursor.style.width = 'auto';
 cursor.style.zIndex = '7';
 cursor.style.position = 'absolute';
@@ -37,47 +51,9 @@ body.appendChild(cursor);
 const moveCursor = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
-   
+    
   cursor.style.top = `${mouseY}px`;
   cursor.style.left = `${mouseX}px`;
 }
 
 window.addEventListener('mousemove', moveCursor);
-
-// document.querySelector('body').style.cursor = 'url(https://i.imgur.com/hlOtvGY.png)';
-
-// const cursor = chrome.runtime.getURL('/faces/angry_ben.jpg');
-// const elems = document.body.getElementsByTagName("*");
-//   for(const i of elems){
-//     i.style.cursor = "url(" + cursor + "), default";
-// }
-
-
-// const angryProfessors = ['https://i.imgur.com/hlOtvGY.png', 'https://i.imgur.com/gg1XNbo.png', 'https://i.imgur.com/Qs3x9xk.png ' ]
-// let currentProf = angryProfessors[0];
-// for (let i = 0; i < )
-// iterate through the images 
-// and change the cursor 
-
-
-// https://i.imgur.com/hlOtvGY.png -- Angry Ben
-// https://i.imgur.com/gg1XNbo.png -- Angry Matt
-// https://i.imgur.com/Qs3x9xk.png -- Angry Stanley 
-// declare a variable storing all the acceptable websites 
-//   const accepted = [
-//     'developer.mozilla',
-//     'github',
-//     'calendar.google',
-//     'w3schools'
-//   ];
-//   if ((document.URL).includes(accepted)) {
-//     alert('Hello World!');
-//     const body = document.querySelector('body');
-//     body.style.cursor = 'no-drop';
-//   }
-// });
-
-/* within this file
-[] identify when someone leaves any of the "acceptable" websites
-[] randomly select instructor 
-*/
--- main.js
+++ main.js
@@ -2,11 +2,35 @@
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   console.log('Tab changed.');
 
-  // Activate anger.js with current tab as target
-  chrome.scripting.executeScript({
-    files: ['anger.js'],
-    target: {
-      tabId: activeInfo.tabId
+  const acceptableSites = [
+    'codesmith',
+    'leetcode',
+    'github',
+    'calendar',
+    'developer.mozilla',
+    'w3schools'
+  ];
+
+  const tab = chrome.tabs.query({
+    "active": true,
+    "windowId": chrome.windows.WINDOW_ID_CURRENT,
+  }).then((tab) => {
+    console.log(tab);
+    let acceptable = false;
+    for (const site of acceptableSites) {
+      
+      if (tab[0].url.includes(site)) acceptable = true;
+    }
+    
+    if (!acceptable) {
+      // Activate anger.js with current tab as target
+      console.log('Script Activated');
+      chrome.scripting.executeScript({
+        files: ['anger.js'],
+        target: {
+          tabId: activeInfo.tabId
+        }
+      });
     }
   });
 });
